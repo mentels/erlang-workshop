b2aa1e0edc8b2db5116ad776adfdb36ca8e58012
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2016-04-21 19:44:20 +0200

    Enable cover; add @docs and type specs to abacus

--- src/abacus.erl
+++ src/abacus.erl
@@ -9,15 +9,31 @@
 
 %% API
 
+%% @doc Adds two integers
+%%
+%% This adds `X' to `Y'.
+-spec addition(integer(), integer()) -> integer().
 addition(X, Y) ->
     X+Y.
 
+%% @doc Subtracts two integers
+%%
+%% This subtracts `Y' from `X'.
+-spec subtraction(integer(), integer()) -> integer().
 subtraction(X, Y) ->
     X-Y.
 
+%% @doc Multiplies two integers
+%%
+%% This multiplies `X' `Y' times.
+-spec multiplication(integer(), integer()) -> integer().
 multiplication(X, Y) ->
     X*Y.
 
+%% @doc Divides two integers
+%%
+%% This multiplies divides `X' by `Y'.
+-spec division(integer(), integer()) -> integer().
 division(X, Y) ->
     X div Y.
 
