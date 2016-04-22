f5bb452a4ae3a510d90e1a1872c8d645380b883c
Szymon Mentel <szymon.mentel@erlang-solutions.com>
2016-04-22 09:48:54 +0200

    Create Erlang Application: add _app and _sup files

---
+++ src/abacus_app.erl
@@ -0,0 +1,16 @@
+-module(abacus_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    abacus_sup:start_link().
+
+stop(_State) ->
+    ok.
---
+++ src/abacus_sup.erl
@@ -0,0 +1,34 @@
+-module(abacus_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type, Args),
+        {I,
+         {I, start_link, Args},
+         permanent,
+         5000,
+         Type,
+         [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    {ok, Port} = application:get_env(abacus, port),
+    {ok, {{one_for_one, 5, 10}, [?CHILD(abacus_srv, worker, [Port])]}}.
+
